cfc065c856a9f54c34b7a4312e84be63a8cba592
Sergio Ruben Giron <sergio_77772@hotmail.com>
2026-03-25 01:21:07 -0300

    ...
    ...

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build' // Configuramos la salida como "build" para que Vercel lo encuentre automáticamente
+  }
 })
